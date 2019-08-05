9115ff8c65b53135d055ac5b3197b42b404308a5
Drew Pepin <drewpepin1995@gmail.com>
2019-08-04 20:28:21 -0400

    Displaying actor gifs to page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,9 +50,40 @@ $(document).ready(function() {
         
     });
 
-    $(actorBtns).on("click", function(event){
-        console.log(event);
-    });
+
+    $("button").on("click", function() {
+        var actor = $(this).attr("class");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          actor + "&api_key=fWfICpURY2bv4yIJEwHUvj6IhgVd2LmX";
+  
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+           var results = response.data;
+           
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $("<div>");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var personImage = $("<img>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+
+                    gifDiv.prepend(p);
+                    gifDiv.prepend(personImage);
+
+                    $("#gifDiv").prepend(gifDiv);
+
+                    console.log(response);
+            };
+
+          });
+
+      });
 
     
 
