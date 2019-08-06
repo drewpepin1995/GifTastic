d4db7db3970f661610f94a2054ab876c6963c8ea
Drew Pepin <drewpepin1995@gmail.com>
2019-08-06 13:33:12 -0400

    Cleaning up JS Code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,13 +31,7 @@ $(document).ready(function () {
 
   };
 
-
-
-
-  renderActors();
-
-  $("#addButton").on("click", function () {
-
+  function renderNewActors() {
 
     if ($("#actorForm").val() === ('')) {
       alert("Please enter an actor!")
@@ -51,66 +45,78 @@ $(document).ready(function () {
       $("#actorBtns").append(newActorButton);
       $("#actorForm").val('');
 
-      $(".newActor").on("click", function () {
-        $("#gifDiv").empty();
-
-        var actor = $(this).attr("class");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          actor + "&api_key=fWfICpURY2bv4yIJEwHUvj6IhgVd2LmX&limit=10";
-
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .then(function (response) {
-            var results = response.data;
+    }
+  };
 
 
-            for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div>");
 
-              var rating = results[i].rating;
 
-              var p = $("<p>").text("Rating: " + rating);
+  renderActors();
 
-              let animated = results[i].images.fixed_height.url;
-              let still = results[i].images.fixed_height_still.url;
+  $("#addButton").on("click", function () {
 
-              var personImage = $("<img>");
-              personImage.attr("src", still);
-              personImage.attr("data-still", still)
-              personImage.attr("data-animate", animated)
-              personImage.attr("data-state", "still");
-              personImage.addClass("actor-image");
+    renderNewActors();
 
-              gifDiv.append(p);
-              gifDiv.append(personImage);
-              gifDiv.addClass('gifImageDiv');
+    $(".newActor").on("click", function () {
+      $("#gifDiv").empty();
 
-              $("#gifDiv").append(gifDiv);
+      var actor = $(this).attr("class");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        actor + "&api_key=fWfICpURY2bv4yIJEwHUvj6IhgVd2LmX&limit=10";
 
-              console.log(response);
-            };
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function (response) {
+          var results = response.data;
 
-            $("img").on("click", function () {
-              let state = $(this).attr("data-state");
 
-              if (state === "still") {
-                $(this).attr("src", $(this).attr("data-animate"));
-                $(this).attr("data-state", "animate");
-              } else {
-                $(this).attr("src", $(this).attr("data-still"));
-                $(this).attr("data-state", "still");
-              }
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
 
-            });
+            var rating = results[i].rating;
 
+            var p = $("<p>").text("Rating: " + rating);
+
+            let animated = results[i].images.fixed_height.url;
+            let still = results[i].images.fixed_height_still.url;
+
+            var personImage = $("<img>");
+            personImage.attr("src", still);
+            personImage.attr("data-still", still)
+            personImage.attr("data-animate", animated)
+            personImage.attr("data-state", "still");
+            personImage.addClass("actor-image");
+
+            gifDiv.append(p);
+            gifDiv.append(personImage);
+            gifDiv.addClass('gifImageDiv');
+
+            $("#gifDiv").append(gifDiv);
+
+            console.log(response);
+          };
+
+          $("img").on("click", function () {
+            let state = $(this).attr("data-state");
+
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
 
           });
 
-      });
 
-    };
+        });
+
+    });
+
+
 
 
 
